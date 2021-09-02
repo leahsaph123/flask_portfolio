4eb2ac0c8db009cf1201dff56ccc51b3b982e986
Tigrans greet <tigranarakelov7@gmail.com>
2021-09-02 14:48:20 -0700

    Tigrans greet

--- main.py
+++ main.py
@@ -43,6 +43,15 @@ def simon():
 def leah():
     return render_template("leah.html")
 
+@app.route('/Tigran', methods=['GET', 'POST'])
+def Tigran():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("Tigran.html", name=name)
+    # starting and empty input default
+    return render_template("Tigran.html", name="World")
 
 @app.route('/isabella/')
 def isabella():
