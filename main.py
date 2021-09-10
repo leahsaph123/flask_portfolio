653bae4c49f4838772b88a70eeb89c9967fb7cab
Chris-Ru <72892271+Chris-Ru@users.noreply.github.com>
2021-09-09 18:53:13 -0700

    fix navbar padding issue on index, and update navbar to only have necessities. Also fixed lag when running project

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 # connects default URL to render index.html
 @app.route('/')
 def index():
-    print("hello")
-    for i in range(0,70):
-        print("hi :3 {}".format(i))
+    #print("hello")
+    #for i in range(0,70):
+        #print("hi :3 {}".format(i))
     return render_template("index.html")
 
 
